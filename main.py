87cd44b006d8dd638d800c05617b9606f9fe066c
Philipp Hager <philipp.konstantin.hager@gmail.com>
2019-06-07 12:05:06 +0200

    Add evaluation for sampled recommendations

--- main.py
+++ main.py
@@ -1,33 +1,63 @@
 import papermill as pm
 
+### Evaluation parameters ###
+sample_users = 100
+recommendations_per_user = 50
+
+### Sample users ###
 pm.execute_notebook(
    "src/sample_users.ipynb",
     "out/sample_users.ipynb",
    parameters = {
        "data": "data/tweets-dedupe.csv",
        "output": "out/users.json",
-       "sample": 100,
+       "sample": sample_users,
    }
 )
 
+### Fetch recommendations from API ###
+# Fetch baseline recommendations
 pm.execute_notebook(
    "src/query_recommendations.ipynb",
-    "out/query_recommendations.ipynb",
+    "out/query_baseline_recommendations.ipynb",
    parameters = {
+       "host": "host.docker.internal",
+       "port": "8070",
        "users": "out/users.json",
-       "output": "out/tweet-recommendations.json",
-       "top_n": 50,
+       "output": "out/baseline-recommendations.json",
+       "top_n": recommendations_per_user,
    }
 )
 
-# Reset evaluation here...
-# Query API again
+# Fetch sampled recommendations
+# TODO: Add other host / port configuration
+pm.execute_notebook(
+   "src/query_recommendations.ipynb",
+    "out/query_sampled_recommendations.ipynb",
+   parameters = {
+       "host": "host.docker.internal",
+       "port": "8070",
+       "users": "out/users.json",
+       "output": "out/sampled-recommendations.json",
+       "top_n": recommendations_per_user,
+   }
+)
 
+### Analysis ###
+# Analyse baseline recommendations
 pm.execute_notebook(
    "src/analyse_recommendations.ipynb",
-    "out/analyse_recommendations.ipynb",
+    "out/analyse_baseline_recommendations.ipynb",
    parameters = {
-       "recommendations": "out/tweet-recommendations.json",
+       "recommendations": "out/baseline-recommendations.json",
    }
 )
 
+# Analyse sampled recommendations
+pm.execute_notebook(
+   "src/analyse_recommendations.ipynb",
+    "out/analyse_sampled_recommendations.ipynb",
+   parameters = {
+       "recommendations": "out/sampled-recommendations.json",
+   }
+)
